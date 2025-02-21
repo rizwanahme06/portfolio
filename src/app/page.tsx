82228957441a01ee2933775a69b7091e232e795b
Rizwan <rizwanahme06@gmail.com>
2025-02-21 21:43:19 +0530

    added footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import Image from "next/image";
 import SkillChart from "@/component/Chart";
 import ProjectList from '@/data/home/projects.json'
-import SocialIcon from '@/data/home/socialIcon.json'
 import Link from "next/link";
 
 const Home = () => {
@@ -97,27 +96,7 @@ const Home = () => {
         </div>
         </div>
       </section>
-      {/* Contact Section */}
-      <section className="bg-siteSecondaryColor py-[40px] md:py-[80px] px-5 text-center">
-        <h1 className="text-2xl font-semibold text-white mb-6">Let&apos;s Work Together</h1>
-        <p className="text-lg text-white mb-8">Got a project in mind? Let&apos;s connect!</p>
-        <div className="flex flex-col md:flex-row justify-center gap-3 max-w-md mx-auto bg-sitePrimaryColor p-4 rounded-lg">
-          <input type="email" placeholder="Your email..." className="bg-transparent outline-none text-white px-4 py-2 w-full" />
-          <button className="bg-siteSecondaryColor py-2 px-4 rounded-md text-white">Let&apos;s Connect</button>
-        </div>
-        <p className="text-lg text-white mt-8">Stay Connected – Let&apos;s Innovate and Collaborate!</p>
-        <div className="flex justify-center gap-6 mt-6">
-          {SocialIcon.SocialIcon.map((data, index) => (
-            <div key={index}>
-            <Link href={data.href} legacyBehavior>
-            <a target="_blank" rel="noopener noreferrer">
-            <Image src={`/images/icon/${data?.icon}.svg`} alt="socialIcon" width={24} height={24} />
-            </a>
-            </Link>
-            </div>
-          ))}
-        </div>
-      </section>
+
     </>
   );
 };
---
+++ src/component/footer/Footer.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import Link from "next/link"
+import Image from "next/image"
+import SocialIcon from '@/data/home/socialIcon.json'
+
+
+export function Footer() {
+    return(
+        <>
+        {/* Contact Section */}
+      <footer className="bg-siteSecondaryColor py-[40px] md:py-[80px] px-5 text-center">
+        <h1 className="text-2xl font-semibold text-white mb-6">Let&apos;s Work Together</h1>
+        <p className="text-lg text-white mb-8">Got a project in mind? Let&apos;s connect!</p>
+        <div className="flex flex-col md:flex-row justify-center gap-3 max-w-md mx-auto bg-sitePrimaryColor p-4 rounded-lg">
+          <input type="email" placeholder="Your email..." className="bg-transparent outline-none text-white px-4 py-2 w-full" />
+          <button className="bg-siteSecondaryColor py-2 px-4 rounded-md text-white">Let&apos;s Connect</button>
+        </div>
+        <p className="text-lg text-white mt-8">Stay Connected – Let&apos;s Innovate and Collaborate!</p>
+        <div className="flex justify-center gap-6 mt-6">
+          {SocialIcon.SocialIcon.map((data, index) => (
+            <div key={index}>
+            <Link href={data.href} legacyBehavior>
+            <a target="_blank" rel="noopener noreferrer">
+            <Image src={`/images/icon/${data?.icon}.svg`} alt="socialIcon" width={24} height={24} />
+            </a>
+            </Link>
+            </div>
+          ))}
+        </div>
+      </footer>
+        </>
+    )
+}
