0beebe370b01d4c7c9625434fdcef585e7e814e5
Rizwan <rizwanahme06@gmail.com>
2025-02-22 19:37:02 +0530

    added number

--- src/component/footer/Footer.tsx
+++ src/component/footer/Footer.tsx
@@ -10,12 +10,15 @@ export function Footer() {
         <>
         {/* Contact Section */}
       <section className="bg-siteSecondaryColor py-[40px] md:py-[80px] px-5 text-center">
-        <h1 className="text-2xl font-semibold text-white mb-6">Let&apos;s Work Together</h1>
+        <div className="container mx-auto p-2 md:px-24">
+        <h2 className="text-2xl font-semibold text-white mb-6">Let&apos;s Work Together</h2>
         <p className="text-lg text-white mb-8">Got a project in mind? Let&apos;s connect!</p>
         <div className="flex flex-col md:flex-row justify-center gap-3 max-w-md mx-auto bg-sitePrimaryColor p-4 rounded-lg">
           <input type="email" placeholder="Your email..." className="bg-transparent outline-none text-white px-4 py-2 w-full" />
           <button className="bg-siteSecondaryColor py-2 px-4 rounded-md text-white">Let&apos;s Connect</button>
         </div>
+        <div className="siteDivider ">OR</div>
+        <Link href="tel:+919321983228" className="siteContactLink">Contact:- +91-9321983228</Link>
         <p className="text-lg text-white mt-8">Stay Connected – Let&apos;s Innovate and Collaborate!</p>
         <div className="flex justify-center gap-6 mt-6">
           {SocialIcon.SocialIcon.map((data, index) => (
@@ -27,6 +30,7 @@ export function Footer() {
             </Link>
             </div>
           ))}
+        </div>
         </div>
       </section>
         </>
